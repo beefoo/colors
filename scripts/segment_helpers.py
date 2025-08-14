ea58d66e53c730d55c043c4a5362e771866ea5d8
beefoo <brian@youaremyjoy.org>
2025-08-14 13:58:33 -0400

    Clean up holes and islands in segments

--- scripts/segment_helpers.py
+++ scripts/segment_helpers.py
@@ -13,6 +13,13 @@ import torch
 from torchvision.transforms import ToTensor
 from PIL import Image
 import requests
+from segment_anything.utils.amg import (
+    batched_mask_to_box,
+    mask_to_rle_pytorch,
+    remove_small_regions,
+    rle_to_mask,
+)
+from torchvision.ops.boxes import batched_nms
      
 def bbox_mask(mask):
     rows = np.any(mask, axis=1)
@@ -33,7 +40,7 @@ def get_image_segment(image, mask):
     bounded_mask = mask[y0: y1, x0: x1]
     bounded_mask_img = Image.fromarray(np.uint8(bounded_mask * 255))
 
-def get_segment_from_point_or_box(pil_image, pts_sampled, pts_labels, model):
+def get_segment_from_point_or_box(pil_image, pts_sampled, pts_labels, model, min_area = 200, nms_thresh = 0.7):
     image_np = np.array(pil_image)
     img_tensor = ToTensor()(image_np)
     pts_sampled = torch.reshape(torch.tensor(pts_sampled), [1, 1, -1, 2])
@@ -48,7 +55,45 @@ def get_segment_from_point_or_box(pil_image, pts_sampled, pts_labels, model):
     predicted_logits = torch.take_along_dim(
         predicted_logits, sorted_ids[..., None, None], dim=2
     )
-    return torch.ge(predicted_logits[0, 0, 0, :, :], 0).cpu().detach().numpy()
+    mask = torch.ge(predicted_logits[0, 0, 0, :, :], 0).cpu().detach().numpy()
+    # return mask
+    masks = process_small_region([np.array([mask])], min_area, nms_thresh)
+    return masks[0]
+
+def process_small_region(masks, min_area = 200, nms_thresh = 0.7):
+    rles = [mask_to_rle_pytorch(torch.from_numpy(m[0:1])) for m in masks]
+    new_masks = []
+    scores = []
+    for rle in rles:
+        mask = rle_to_mask(rle[0])
+
+        mask, changed = remove_small_regions(mask, min_area, mode="holes")
+        unchanged = not changed
+        mask, changed = remove_small_regions(mask, min_area, mode="islands")
+        unchanged = unchanged and not changed
+
+        new_masks.append(torch.as_tensor(mask).unsqueeze(0))
+        # Give score=0 to changed masks and score=1 to unchanged masks
+        # so NMS will prefer ones that didn't need postprocessing
+        scores.append(float(unchanged))
+
+    # Recalculate boxes and remove any new duplicates
+    masks = torch.cat(new_masks, dim=0)
+    boxes = batched_mask_to_box(masks)
+    keep_by_nms = batched_nms(
+        boxes.float(),
+        torch.as_tensor(scores),
+        torch.zeros_like(boxes[:, 0]),  # categories
+        iou_threshold=nms_thresh,
+    )
+
+    # Only recalculate RLEs for masks that have changed
+    for i_mask in keep_by_nms:
+        if scores[i_mask] == 0.0:
+            mask_torch = masks[i_mask].unsqueeze(0)
+            rles[i_mask] = mask_to_rle_pytorch(mask_torch)
+    masks = [rle_to_mask(rles[i][0]) for i in keep_by_nms]
+    return masks
 
 def show_anns(mask, ax):
     ax.set_autoscale_on(False)
@@ -75,7 +120,7 @@ def show_mask(mask, ax, random_color=False):
     ax.imshow(mask_image)
 
 
-def show_points(coords, ax, marker_size=600):
+def show_points(coords, ax, marker_size=1200):
     ax.scatter(
         coords[:, 0],
         coords[:, 1],
